9368010cb7ede117d0a4835f778bbaf80952307c
Samridh Tuladhar <t.samridh.86@gmail.com>
2017-03-15 09:26:04 +0530

    removed extra content

--- config/config.php
+++ config/config.php
@@ -62,27 +62,5 @@ class connector
     else return "others";
   }
 
-  //this function calculates the number of digits of a number, this is used the algorithms below
-	function numberOfDigits($num)
-	{
-		if($num == 1 ) return 1;
-		 return ceil(log10($num));
-		
-	}
-
-  //this is an extra function to calculate the name of the image to be replaced during the server upload
-  function nameOfImage ( $fileName) 
-  {
-  	$i = 0;
-		while(file_exists("../images/".$fileName))
-		{
-			if(!$i) 
-			$fileName = substr($fileName,0,-4).$i.substr($fileName, -4);
-			else 	 
-				$fileName= substr($fileName,0,-(numberOfDigits($i)+4)).$i.substr($fileName, -4);
-			$i++;
-		}
-		return $fileName;
-  }
-
+  
 ?>
